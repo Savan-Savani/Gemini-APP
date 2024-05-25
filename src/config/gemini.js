5b15611ab392abf63fc818de021a0a68951cb0bb
Savan <savansavani111@gmail.com>
2024-05-25 10:05:56 -0400

    complete 💯

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -13,7 +13,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = GOOGLE_GEMINI_API_KEY;
+const apiKey = import.meta.env.VITE_GOOGLE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
